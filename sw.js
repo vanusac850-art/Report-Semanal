8b9b2deac41f81b536695f8378ec68830ab65d89
vanusac850-art <vanusac850@gmail.com>
2026-06-16 16:57:55 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+// Unregister all previous caches and service workers
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
